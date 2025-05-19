cd8fd36d9e7eb001b69eb357392a8f8fc51d4ebf
Michael Danylchuk <mn_danylchuk@me.com>
2025-05-18 22:42:27 -0700

    Title: Refactor Key Press Handler to Incorporate Debounce Logic

--- .vscode/Testcode.py
+++ .vscode/Testcode.py
@@ -1,7 +1,29 @@
-def process_note_on(note):
-    print(f"Note ON: {note}")
+import time
 
-def process_note_off(note):
-    print(f"Note OFF: {note}")
+class KeyPressHandler:
+    def __init__(self):
+        self.last_key_press_time = {}
+        self.debounce_time = 0.05  # debounce time in seconds
+
+    def handle_key_press(self, key_event):
+        current_time = time.time()
+        last_press_time = self.last_key_press_time.get(key_event.note, 0)
+        
+        # If key press event happened within debounce time, ignore it
+        if current_time - last_press_time < self.debounce_time:
+            return
+        
+        self.last_key_press_time[key_event.note] = current_time
+
+        if key_event.pressed:
+            self.process_note_on(key_event.note)
+        else:
+            self.process_note_off(key_event.note)
+
+    def process_note_on(self, note):
+        print(f"Note ON: {note}")
+
+    def process_note_off(self, note):
+        print(f"Note OFF: {note}")
 
 # Something New
